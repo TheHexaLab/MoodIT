ea69ffdbb92d3921ccc862fc625a45b98fd49376
Rosie <rosiehomierg@gmail.com>
2026-06-04 17:08:44 -0400

    model v4

---
+++ core-service/src/main/java/com/moodit/core_service/model/Course.java
@@ -0,0 +1,28 @@
+package com.moodit.core_service.model;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_course")
+    private Integer id;
+
+    @Column(length = 128)
+    private String title;
+
+    @Column(length = 256)
+    private String description;
+
+    @Column(nullable = false, length = 128)
+    private String code;
+
+    @ManyToMany(mappedBy = "courses")
+    private List<Program> programs;
+
+    
+}
---
+++ core-service/src/main/java/com/moodit/core_service/model/User.java
@@ -0,0 +1,60 @@
+package com.moodit.core_service.model;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "user_")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 64)
+    private String username;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true, length = 256)
+    private String email;
+
+    @Column(columnDefinition = "TEXT")
+    private String settings;
+
+    @Column(name = "avatar_color", length = 9)
+    private String avatarColor = "#0a5cc0";
+
+    @Column(name = "active_token_hash", length = 256)
+    private String activeTokenHash;
+
+    @Column(name = "password_hash", nullable = false, length = 256)
+    private String passwordHash;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "verified_email")
+    private Boolean verifiedEmail;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<Role> roles;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_program",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "program_id")
+    )
+    private List<Program> programs;
+}
