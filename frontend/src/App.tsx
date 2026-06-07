5832d8064017a8f7aaf29cc5a37aebac316e6edf
Michaël Normand <michael.normand@hotmail.com>
2026-06-07 19:28:18 -0400

    Mise à jours de App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 //import ServiceStatus from './pages/ServiceStatus';
+//import PopupTests from './pages/PopupTests.tsx';
 import Dashboard from './pages/Dashboard/Dashboard.tsx';
 
 export default function App() {
   //return <ServiceStatus />;
+  //return <PopupTests />
   return <Dashboard />;
 }
